7778397c0ee6bddc39bbbcff913979a8ab92ef0d
Dave Taylor <tayloredwebsites@me.com>
2025-02-16 14:08:45 -0500

    initial commit after cookiecutter

---
+++ healthymeals/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from healthymeals.users.models import User
+from healthymeals.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
